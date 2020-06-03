b29f07df59b400645e7cdf8e0251629dd2b16453
Domingos Lequechane <domingosf.lequechane@gmail.com>
2020-06-03 18:07:38 +0200

    Dia 2: Criação da página de cadastro de ponto de colecta

---
+++ scripts/scripts.js
@@ -0,0 +1,78 @@
+// * // COMENTARIOS
+// document.write("Ola Mundo")
+// // o document é um documento assim como o html, 
+// // write é a funça o que o documento "document" tem
+
+
+
+// * // VARIAVEIS, CONSTANTES TIPOS DE DADOS
+
+// * // variaveis e comstantes
+// let myvar = "He"
+// const myconst = "He"
+// document.write(myconst + myvar)
+// // uma variavel serve para registrar em memoria alguma coisa para usar mais tarde.
+
+// * // string
+// "Isso é uma string"
+// 'Isso também é uma string'
+// `Isso é uma string também`
+
+// * // number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+
+// * // boolean - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+
+
+// * // ESTRUTURA DE DADOS
+
+// * // objectoto é tudo aquilo que possui propriedades e funcionalidade
+// const pessoa = {
+//   * // propriedades
+//   altura: "1.80m",
+//   idade: 24,
+//   solteiro: true,
+//   * // funcionalidades
+//   correr() {
+//     return "run Forest"
+//   }
+// }
+// document.write(pessoa.correr())
+
+// * // arrays - vectores
+// // arrey é uma coleção de dados, ex: bolinhas
+// const colecaoDeBolinhas = ["blue", "green", 1, {name: "My name"}]
+// document.write(colecaoDeBolinhas[3].name)
+// // [0] refere-se a posicao do objecto a acessar
+// // ao acessarmos a posicao tres, pelo facto de ser um objecto, esta precisa ser acessada pelo .name
+
+// * // FUNÇÕES=FUNCIONALIDADE=ATALHOS=REUSO DE CODIGO
+//   * // registrar função
+// function sayMayName(name) {
+//   document.write(name)
+// }
+// // acima, temos uma funcao que ja foi registrada mas ainda nao foi usada
+//  * // executar
+// sayMayName("Domingos")
+// sayMayName("Francisco")
+// sayMayName("Lequechane")
+
+// // condicionais
+// const notaFinal = 8
+
+// if (notaFinal < 5) {
+//   document.write("Reprovado")
+// } else {
+//   document.write("Aprovado")
+// }
+
+// * // loop - repetições
+// for (i = 0 ; i < 10; i++) {
+//   document.write(`<p>${i}</p>`)
+// }
